c1c2f52056b7fa27dfb12313add242d2fb9cb11a
cry004 <cry004@gmail.com>
2018-05-05 23:08:56 +0900

    Vue cycle

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h1> {{ status }} </h1>
+  <app-server></app-server>
+</div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return {
+      status: 'AAAA'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
