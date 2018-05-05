ee41337b8bac0b238064630f1c89c8d67a6eb6d7
cry004 <cry004@gmail.com>
2018-05-06 00:12:35 +0900

    Components export, import

---
+++ Component_practice/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <appHeader></appHeader>
+        <hr>
+          <Servers></Servers>
+        <hr>
+          <app-footer></app-footer>
+    </div>
+</template>
+
+<script>
+  import Header from './components/Shared/Header.vue';
+  import Servers from './components/Servers/Servers.vue';
+  import Footer from './components/Shared/Footer.vue';
+
+  export default {
+    components: {
+      appHeader: Header,  //vue js
+      Servers,  //es6
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>
