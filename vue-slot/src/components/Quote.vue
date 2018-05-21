fe14048182761c5c14f4e14f400028dda786491f
daifenglee <daifen.lee@monstar-lab.com>
2018-05-22 00:01:38 +0900

    Slot function

---
+++ vue-slot/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div>
+    <slot name="title"></slot>
+    <span style="color: #ccc"><slot name="subtitle">The subtitle</slot></span>
+  </div>
+  </hr>
+  <div>
+    <slot></slot>
+  </div>
+</div>
+</template>
+<script>
+  export default {
+
+  }
+</script>
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 30px;
+    margin: 30px auto;
+    text-align: center;
+  }
+  h2 {
+   color: red;
+   font-weight: bolder;
+  }
+</style>
