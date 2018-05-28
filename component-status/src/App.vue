722be809b54d6e9d5d7871916a3700192518b5f6
daifenlee <daifen.lee@monstar-lab.com>
2018-05-28 23:57:05 +0900

    slot&component advanced

---
+++ component-status/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <br>
+                <button class="btn btn-primary" @click="colornow = 'appBlue'">Load Blue Template</button>
+                <button class="btn btn-success" @click="colornow = 'appGreen'">Load Green Template</button>
+                <button class="btn btn-danger" @click="colornow = 'appRed'">Load Red Template</button>
+                <hr>
+                <component :is="colornow">
+                  <p>This is Content</p>
+                </component>
+                <!--<app-blue></app-blue>-->
+                <!--<app-green></app-green>-->
+                <!--<app-red></app-red>-->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Blue from './components/Blue.vue';
+    import Green from './components/Green.vue';
+    import Red from './components/Red.vue';
+
+    export default {
+        data() {
+          return {
+          colornow: 'appBlue'
+          }
+        },
+        components: {
+            appBlue: Blue,
+            appGreen: Green,
+            appRed: Red
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ vue-slot/src/components/New.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <h3>New Quote</h3>
+  <slot></slot>
+  <button @click="counter++"> increase! </button>
+  <p>{{ counter }}</p>
+</div>
+</template>
+<script>
+  export default {
+  data: function(){
+    return {
+      counter: 0
+    }
+  },
+   destroyed(){
+    console.log('destrory')
+   },
+   deactivated(){
+    console.log('deactivated')
+   },
+   activated(){
+    console.log('activated')
+   }
+  }
+</script>
+<style>
+</style>
