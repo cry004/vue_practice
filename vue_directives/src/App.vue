5c19dea60f98e11c9fb2b7991a439bec497baf33
cry004 <cry004@gmail.com>
2018-07-10 21:37:54 +0800

    another way manage modifier

--- vue_directives/src/App.vue
+++ vue_directives/src/App.vue
@@ -14,7 +14,8 @@
                 <!--p v-highlight>Color this</p-->
                 <p v-highlight="'red'">How to color this</p>
                 <p v-highlight:background="'red'">Color this</p>
-                <p v-local-highlight:background.delayed.blink="'red'">Color this</p>
+                <!--p v-local-highlight:background.delayed.blink="'red'">Color this</p-->
+                <p v-local-highlight:background.delayed.blink="{mainColor: 'red', subColor: 'green', delay: 500}">Color this</p>
             </div>
         </div>
     </div>
@@ -30,8 +31,8 @@ export default {
           delay = 3000;
         }
         if (binding.modifiers['blink']) {
-          let mainColor = binding.value;
-          let subColor = 'blue';
+          let mainColor = binding.value.mainColor;
+          let subColor = binding.value.subColor;
           let currentColor = mainColor;
           setTimeout(() => {
             setInterval(() => {
@@ -43,14 +44,14 @@ export default {
               } else {
                 el.style.color = currentColor;
               }
-            }, 1000);
+            }, binding.value.delay);
           }, delay);
         } else {
           setTimeout(() => {
             if (binding.arg == 'background') {
-              el.style.backgroundColor = binding.value;
+              el.style.backgroundColor = binding.value.mainColor;
             } else {
-              el.style.color = binding.value;
+              el.style.color = binding.value.mainColor;
             }
           }, delay);
         }
