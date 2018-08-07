f4d1147c8f4518c0daa1c9eb98bee234fc2c452b
cry004 <cry004@gmail.com>
2018-08-07 23:50:30 +0800

    firebase

--- vue_resource/src/App.vue
+++ vue_resource/src/App.vue
@@ -3,15 +3,29 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                  <label>Username</label>
+                  <input class="form-control" type="text" v-model="user.username">
+                </div>
+                <div class="form-group">
+                  <label>Mail</label>
+                  <input class="form-control" type="text" v-emodel="user.rmail">
+                </div>
+                <button class="btn btn-primary">submit!!</button>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-    }
+export default {
+  data() {
+    return {
+      username: '',
+      email: ''
+    };
+  }
+};
 </script>
-
 <style>
 </style>
