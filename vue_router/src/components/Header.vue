09d9d1b489f75a67e0c15c0df4f5521da8430c47
cry004 <cry004@gmail.com>
2018-08-26 17:46:37 +0800

    if user/id

---
+++ vue_router/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="nav nav-pills">
+    <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link></li>
+    <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link></li>
+    <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link></li>
+  </ul>
+</template>
--- vue_router/src/routes.js
+++ vue_router/src/routes.js
@@ -2,6 +2,6 @@ import User from './components/user/User.vue'
 import Home from './components/Home.vue'
 
 export const routes = [
-  { path: '/user', component: User },
-  { path: '', component: Home }
+  { path: '', component: Home },
+  { path: '/user/:id', component: User }
 ]
