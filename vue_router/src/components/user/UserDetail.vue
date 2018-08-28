30516343dc51abbb71bb511b6afde33dcadf34fc
cry004 <cry004@gmail.com>
2018-08-28 15:01:26 +0800

    animation,  hash

--- vue_router/src/components/user/UserDetail.vue
+++ vue_router/src/components/user/UserDetail.vue
@@ -3,10 +3,21 @@
     <h3>Some User Details</h3>
     <p>User loaded has ID: {{ $route.params.id }}</p>
     <router-link tag="button"
-                 :to="{ name: 'userEdit',
-                        params: { id: $route.params.id },
-                        query: { local: 'en', q:100 }
-               }"
+                 :to="link"
                  >Edit user</router-link>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      link: {
+        name: 'userEdit',
+        params: { id: this.$route.params.id },
+        query: { local: 'en', q: 100 },
+        hash: '#data'
+      }
+    };
+  }
+};
+</script>
